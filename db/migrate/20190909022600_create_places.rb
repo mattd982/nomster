2c27e6885024f8b0803291918d40beaaef284687
matt <mattdxc982@gmail.com>
2019-09-09 16:11:35 +0000

    look over

--- db/migrate/20190909022600_create_places.rb
+++ db/migrate/20190909022600_create_places.rb
@@ -1,4 +1,4 @@
-class CreatePlaces < ActiveRecord::Migration[5.2]
+class CreatePlaces < ActiveRecord::Migration[5.0]
   def change
     create_table :places do |t|
       t.string :name
@@ -7,4 +7,4 @@ class CreatePlaces < ActiveRecord::Migration[5.2]
       t.timestamps
     end
   end
- end
+end
