e2c44867013b2086b00410e9042688fdb503eb85
matt <mattdxc982@gmail.com>
2019-09-22 19:00:39 +0000

    place detail page

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -12,11 +12,14 @@ class PlacesController < ApplicationController
         current_user.places.create(place_params)
     redirect_to root_path
   end
+  
+  def show
+    @place = Place.find(params[:id])
+  end
 
   private
 
   def place_params
     params.require(:place).permit(:name, :description, :address)
     end
-
-end
+   end
