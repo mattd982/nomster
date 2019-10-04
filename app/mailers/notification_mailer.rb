e9aa4f8ca5f0ba53d6856ddd3a7667e058dff389
matt <mattdxc982@gmail.com>
2019-10-04 03:38:09 +0000

    test email send

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,7 +2,7 @@ class NotificationMailer < ApplicationMailer
   default from: "no-reply@nomsterapp.com"
   
     def comment_added
-        mail(to: "mattdxc982@gmail.com"
+        mail(to: "codingmd982@gmail.com",
             subject: "A comment has been added to your place")
   end
 end
