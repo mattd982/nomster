13716f8633f3a0d5f7f7bede4193cc50aa14fe49
matt <mattdxc982@gmail.com>
2019-10-09 14:28:14 +0000

    edit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,8 +4,8 @@ Rails.application.configure do
     address: 'smtp.sendgrid.net',
     port: '587',
     authentication: :plain,
-    user_name: ENV['SENDGRID_USERNAME'],
-    password: ENV['SENDGRID_PASSWORD'],
+    user_name: ENV['SENDGRID_firehawk982@gmail.com'],
+    password: ENV['SENDGRID_Hitman@982'],
     domain: 'heroku.com',
     enable_starttls_auto: true
   }
