fa31bdf599121bb982313c2fa805f77ed577e7cb
Matt Day <mattdxc982@gmail.com>
2019-10-21 14:46:06 +0000

    photo

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -5,12 +5,15 @@ class PhotosController < ApplicationController
     @place = Place.find(params[:place_id])
     @place.photos.create(photo_params.merge(user: current_user))
     redirect_to place_path(@place)
-    
-    end
+ end
+ 
+   def show
+     @photo = Photo.find(params[:id])
+   end
     
     private
     
     def photo_params
-     params.require(:photo).permit(:caption)
+     params.require(:photo).permit(:caption, :picture)
   end
 end
